781abb88082576d4ee127d3ac1403cf3e95173c6
Toridesu <160250540+Toridesu@users.noreply.github.com>
2024-11-11 20:00:49 +0900

    Footer、DefaultLayoutの追加

--- React_Study_02/Atomic Design/src/App.js
+++ React_Study_02/Atomic Design/src/App.js
@@ -2,6 +2,7 @@ import { PrimaryButton } from './components/atoms/button/PrimaryButton';
 import { SecondaryButton } from './components/atoms/button/SecondaryButton';
 import { SearchInput } from './components/molecules/SearchInput';
 import { UserCard } from './components/organism/user/UserCard';
+import { DefaultLayout } from './components/templates/DefaultLayout';
 import { HeaderOnly } from './components/templates/HeaderOnly';
 import { BrowserRouter } from 'react-router-dom';
 import './App.css';
@@ -20,13 +21,13 @@ const user = {
 function App() {
   return (
     <BrowserRouter>
-      <HeaderOnly>
+      <DefaultLayout>
         <PrimaryButton>テスト</PrimaryButton>
         <SecondaryButton>検索</SecondaryButton>
         <br />
         <SearchInput />
         <UserCard user={user} />
-      </HeaderOnly>
+      </DefaultLayout>
     </BrowserRouter>
   );
 }
