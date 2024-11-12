b00b7906eb6fc009c1751da3493877a21baebda1
Toridesu <160250540+Toridesu@users.noreply.github.com>
2024-11-12 12:24:16 +0900

    Userカードの実装

--- React_Study_02/Atomic Design/src/App.js
+++ React_Study_02/Atomic Design/src/App.js
@@ -1,22 +1,8 @@
-
 import './App.css';
 import { Router } from './router/Router';
 
-const user = {
-  name: 'とり',
-  image: '/nadi-whatisdelirium-fZ8uf_L52wg-unsplash.jpg',
-  email: '12345@example.com',
-  phone: '090-1111-2222',
-  company: {
-    name: 'テスト株式会社',
-  },
-  website: 'https://google.com',
-};
-
 function App() {
-  return (
-    <Router />
-  );
+  return <Router />;
 }
 
 export default App;
