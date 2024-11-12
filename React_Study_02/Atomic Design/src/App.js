ea32b8c65051812a4a1fa41bef2cc634004f1434
Toridesu <160250540+Toridesu@users.noreply.github.com>
2024-11-12 11:51:27 +0900

    Router

--- React_Study_02/Atomic Design/src/App.js
+++ React_Study_02/Atomic Design/src/App.js
@@ -1,11 +1,6 @@
-import { PrimaryButton } from './components/atoms/button/PrimaryButton';
-import { SecondaryButton } from './components/atoms/button/SecondaryButton';
-import { SearchInput } from './components/molecules/SearchInput';
-import { UserCard } from './components/organism/user/UserCard';
-import { DefaultLayout } from './components/templates/DefaultLayout';
-import { HeaderOnly } from './components/templates/HeaderOnly';
-import { BrowserRouter } from 'react-router-dom';
+
 import './App.css';
+import { Router } from './router/Router';
 
 const user = {
   name: 'とり',
@@ -20,15 +15,7 @@ const user = {
 
 function App() {
   return (
-    <BrowserRouter>
-      <DefaultLayout>
-        <PrimaryButton>テスト</PrimaryButton>
-        <SecondaryButton>検索</SecondaryButton>
-        <br />
-        <SearchInput />
-        <UserCard user={user} />
-      </DefaultLayout>
-    </BrowserRouter>
+    <Router />
   );
 }
 
