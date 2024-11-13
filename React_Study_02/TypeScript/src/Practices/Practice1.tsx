3e2b51b502bdedf21c719b52de5ab0496f71ba96
Toridesu <160250540+Toridesu@users.noreply.github.com>
2024-11-13 12:39:41 +0900

    税率計算の実装

--- React_Study_02/TypeScript/src/Practices/Practice1.tsx
+++ React_Study_02/TypeScript/src/Practices/Practice1.tsx
@@ -1,11 +1,30 @@
-import React from "react";
+import { useState } from 'react';
 
 export const Practice1 = () => {
-  const onclickPractice = () => alert("")
-  return(
+  const [inputValue, setInputValue] = useState<number>(0);
+  const [total, setTotal] = useState<number>(0);
+  const [taxRate, setTaxRate] = useState<number>(0);
+
+  const calcTotalFee = (num: number) => {
+    const calculatedTotal = num * (1 + taxRate / 100);
+    setTotal(calculatedTotal);
+  };
+  const onclickPractice = () => calcTotalFee(inputValue);
+
+  return (
     <div>
-      <p>練習問題:引数の型指定</p>
-      <button onClick={onclickPractice}>練習問題1を実行</button>
+      <h2>税率計算</h2>
+      <div>
+        <label>金額：</label>
+        <input type='number' value={inputValue} onChange={(e) => setInputValue(Number(e.target.value))} />
+      </div>
+      <div>
+        <label>税率（%）：</label>
+        <input type='number' value={taxRate} onChange={(e) => setTaxRate(Number(e.target.value))} />
+      </div>
+      <button onClick={onclickPractice}>計算</button>
+      <br />
+      <p>合計: {total}円</p>
     </div>
-  )
-}
+  );
+};
