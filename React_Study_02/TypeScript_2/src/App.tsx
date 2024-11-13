f50c76ac7cbafa9555abac7fbdd4f0be17850b5e
Toridesu <160250540+Toridesu@users.noreply.github.com>
2024-11-13 15:16:02 +0900

    TypeScriptの型定義

--- React_Study_02/TypeScript_2/src/App.tsx
+++ React_Study_02/TypeScript_2/src/App.tsx
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import axios from 'axios';
+import { Todo } from './Todo';
+
+type TodoType = {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+};
 
 function App() {
-  return (
-    <div className="App">
+  const [todos, setTodos] = useState<Array<TodoType>>([]);
 
+  const onClickFetchData = () => {
+    axios.get<Array<TodoType>>('https://jsonplaceholder.typicode.com/todos').then((res) => {
+      setTodos(res.data);
+    });
+  };
+
+  return (
+    <div className='App'>
+      <button onClick={onClickFetchData}>データ取得</button>
+      {todos.map((todo: TodoType) => (
+        <Todo key={todo.id} title={todo.title} userId={todo.userId} completed={todo.completed} />
+      ))}
     </div>
   );
 }
